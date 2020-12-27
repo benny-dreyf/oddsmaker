69ba6b0241a39417cd9e2e3b2027d72d573263a7
benny-dreyf <bendreyfuss@fastmail.com>
2020-12-27 11:10:42 -0500

    updated package to work off of purrr instead of forloops

--- R/odds_consensus.R
+++ R/odds_consensus.R
@@ -12,40 +12,35 @@
 #' @importFrom magrittr %>%
 #'
 odds<- function(week_num, num_games){
-  # create the list of games
   games<- c(2:(num_games+1))
-  # create a list of html for each game at the url
-  game_list<- purrr::map(.x= '', paste, '#block-system-main > div > div:nth-child(', games, ') > table',
-                         sep = "")
-  # create a blank tibble to start
+  url_list<- purrr::map(.x= '', paste, '#block-system-main > div > div:nth-child(', games, ') > table',
+                        sep = "")
   data_pull<- tibble::tibble()
-  for (i in seq_along(game_list[[1]])){
-    tmp<- xml2::read_html('https://www.oddsshark.com/nfl/consensus-picks') %>%
-      rvest::html_node(game_list[[1]][i]) %>%
-      rvest::html_table() %>%
-      dplyr::mutate_all(as.character)
-    data_pull<- dplyr::bind_rows(data_pull, tmp)
+  table_gather<- function(item){
+    xml2::read_html('https://www.oddsshark.com/nfl/consensus-picks') %>%
+      rvest::html_node(item) %>%
+      rvest::html_table()
   }
-  data_pull %>%
-    dplyr::select(team= X2, spread_share= X3, spread= X4, spread_payout= X5,
-                  ou= X6, ou_share=X7, ou_payout=X8) %>%
-    dplyr::mutate(spread_share= as.numeric(stringr::str_replace(spread_share, '%', ""))/100,
-                  ou_share= as.numeric(stringr::str_replace(ou_share, '%', ""))/100,
-                  ou= as.numeric(stringr::str_replace(ou, 'O/U', "")),
-                  date_pulled= lubridate::today(tzone = 'EST')) %>%
-    dplyr::mutate(game_num= as.numeric(seq(from= 1, to= NROW(data_pull)))) %>%
-    dplyr::mutate(home_away= dplyr::case_when(game_num %% 2 == 0 ~ 'home', TRUE ~ 'away'),
-                  game_num= dplyr::case_when(game_num %% 2 == 0 ~ game_num/2, TRUE ~ game_num),
-                  game_num= dplyr::case_when(game_num > dplyr::lead(game_num, n= 1) ~ dplyr::lead(game_num, n=1), TRUE ~ game_num),
-                  game_num= dplyr::case_when(lubridate::wday(Sys.Date()) > 4 ~ game_num + 1,
-                                             T ~ game_num),
+  final_table<- url_list[[1]] %>%
+    purrr::map(.f = table_gather) %>%
+    purrr::map(dplyr::mutate_all, as.character) %>%
+    dplyr::bind_rows(.id = 'game_num') %>%
+    dplyr::select(game_num, team= X2, spread_share= X3, spread= X4, spread_payout= X5,
+                  ou= X6, ou_share= X7, ou_payout= X8) %>%
+    dplyr::mutate(date_pulled= lubridate::today(tzone = 'EST'),
+                  team= stringr::str_extract(team, '^([A-Z]{3}|[A-Z]{2})'),
+                  game_num= as.numeric(game_num),
+                  home_away= dplyr::case_when(game_num %% 2 == 0 ~ 'home', TRUE ~ 'away'),
                   week= paste('Week', week_num, sep = " "),
+                  spread_share= as.numeric(stringr::str_replace(spread_share, '%', ""))/100,
                   spread_payout = dplyr::case_when(as.double(spread_payout) > 0 ~ as.double(spread_payout) * -1,
                                                    TRUE ~ as.double(spread_payout)),
+                  ou_share= as.numeric(stringr::str_replace(ou_share, '%', ""))/100,
+                  ou= as.numeric(stringr::str_replace(ou, 'O/U', "")),
                   ou_payout = dplyr::case_when(as.double(ou_payout) > 0 ~ as.double(ou_payout) * -1,
-                                               TRUE ~ as.double(ou_payout)),
-                  team= str_extract(team, '^([A-Z]{3}|[A-Z]{2})')) %>%
+                                               TRUE ~ as.double(ou_payout))) %>%
     dplyr::select(date_pulled, week, game_num, team, home_away, dplyr::everything()) %>%
     dplyr::mutate_at(dplyr::vars(6:11), as.double) %>%
     dplyr::mutate_if(is.numeric, ~replace(., is.na(.), 0))
+  return(final_table)
 }
