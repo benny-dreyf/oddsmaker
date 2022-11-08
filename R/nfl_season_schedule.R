99ea68c590e896846f3ab5fe30fb0990a00ede1e
benny-dreyf <bendreyfuss@fastmail.com>
2022-11-07 22:58:52 -0500

    adding lookup and finalizing namespace

--- R/nfl_season_schedule.R
+++ R/nfl_season_schedule.R
@@ -20,14 +20,14 @@ nfl_season_schedule<-function(year){
     dplyr::select(week, day, date, time, away= winner_tie, home= loser_tie) |>
     dplyr::group_by(week) |>
     dplyr::group_split() |>
-    purrr::map(dplyr::mutate, game_num= row_number()) |>
+    purrr::map(dplyr::mutate, game_num= dplyr::row_number()) |>
     dplyr::bind_rows() |>
     tidyr::unite(col = 'game_time', date, time, sep= " ") |>
     dplyr::mutate(game_time= lubridate::ymd_hm(game_time),
-           game_time= lubridate::as_datetime(format(game_time, format="%Y-%m-%d %I:%M:%S"))) |>
+                  game_time= lubridate::as_datetime(format(game_time, format="%Y-%m-%d %I:%M:%S"))) |>
     tidyr::pivot_longer(cols= -c('week', 'day', 'game_time', 'game_num'), names_to = 'home_away', values_to = 'team') |>
     dplyr::select(week, day, game_time, game_num, home_away, team) |>
-    dplyr::left_join(read_csv('team_abbrev_match.csv'), by= 'team') |>
+    dplyr::left_join(readr::read_csv('team_abbrev_match.csv'), by= 'team') |>
     dplyr::rename(team_abbrev= abrev) |>
     dplyr::select(week, day, game_time, game_num, team, team_abbrev, home_away)
   return(sched)
